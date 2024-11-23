9917ac6aec6dc266fcd54b82d0aad4821f81faa5
Xu Peng Yan <xupeng.yan@gmail.com>
2024-11-23 01:58:38 -0800

    added react-quill-new

--- src/app/document/page.tsx
+++ src/app/document/page.tsx
@@ -1,20 +1,22 @@
 'use client'
 import React from 'react';
 import model from '../../db/jsonjoy'
+import ReactQuillNew from '../components/ReactQuillNew'
 
 export default function Document(){
     const view = React.useSyncExternalStore(
         model.api.subscribe,
         model.api.getSnapshot, [model] as any);
-      
-      return (
-        <>
-        <div>
-            {String(model.api.node.get('format').view())}
-        </div>
-        <div>
-            {String(model.api.node.get('text').view())}
-        </div>
-        </>
-      )
+    
+    return (
+    <>
+    <div>
+        {String(model.api.node.get('format').view())}
+    </div>
+    <div>
+        {String(model.api.node.get('text').view())}
+    </div>
+    <ReactQuillNew />
+    </>
+    )
 }
