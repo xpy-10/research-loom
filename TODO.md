5b38137a0d0968b80f5da1a4aab09277134f48d7
Xu Peng Yan <xupeng.yan@gmail.com>
2024-11-22 22:22:48 -0800

    testing json-joy package as crdt provider with react

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 [X] Xata database connection
 [X] Drizzle integration
 [X] Vercel Integration
-[ ] Upload Thing Integration
 [X] Clerk Auth integration
---
+++ src/app/document/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import model from '../../db/jsonjoy'
+
+export default function Document(){
+    const view = React.useSyncExternalStore(
+        model.api.subscribe,
+        model.api.getSnapshot, [model] as any);
+      
+      return (
+        <>
+        <div>
+            {String(model.api.node.get('format').view())}
+        </div>
+        <div>
+            {String(model.api.node.get('text').view())}
+        </div>
+        </>
+      )
+}
