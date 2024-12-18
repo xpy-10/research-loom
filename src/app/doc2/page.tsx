35d34c05645793bc806879a8b8b882f876c1e477
Xu Peng Yan <xupeng.yan@gmail.com>
2024-12-18 01:50:27 -0800

    test out using react-quilljs package for server side rendering safety

---
+++ src/app/doc2/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import { useQuill } from 'react-quilljs';
+// or const { useQuill } = require('react-quilljs');
+
+import 'quill/dist/quill.snow.css'; // Add css for snow theme
+// or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
+
+export default () => {
+  const { quill, quillRef } = useQuill();
+
+  console.log(quill);    // undefined > Quill Object
+  console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
+
+  return (
+    <div style={{ width: 500, height: 300 }}>
+      <div ref={quillRef} />
+    </div>
+  );
+};
